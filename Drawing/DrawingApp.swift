f96329dcdd8c7097de90b7373f6f0be6c415837d
Ivan Trj <ivantrajanovski@gmail.com>
2023-06-20 10:57:19 +0200

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Ivan Trajanovski on 20.06.23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
